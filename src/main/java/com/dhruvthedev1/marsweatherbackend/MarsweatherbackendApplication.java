e70d50f38bd4a17b1ace7924bd9d29aee54c8aa7
DhruvTheDev1 <dhruv2015@hotmail.co.uk>
2025-08-13 15:08:11 +0100

    Initial commit for backend

---
+++ src/main/java/com/dhruvthedev1/marsweatherbackend/MarsweatherbackendApplication.java
@@ -0,0 +1,13 @@
+package com.dhruvthedev1.marsweatherbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsweatherbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsweatherbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhruvthedev1/marsweatherbackend/MarsweatherbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhruvthedev1.marsweatherbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsweatherbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
